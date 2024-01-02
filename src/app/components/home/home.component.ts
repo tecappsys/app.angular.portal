b120fe2f193ffefe9ceee0f7fcfb84da0008a772
Daniel <danieldiazmc@gmail.com>
2024-01-02 16:43:56 -0300

    update files

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { GLOBALS_PATH } from '@src/app/shared/enums/globals-path.enum';
 
 @Component({
   selector: 'app-home',
@@ -14,14 +15,15 @@ export class HomeComponent {
       imgPath:'../../assets/img/angular_spa_app.jpg',
       imgLogoPath:'../../assets/img/angular_spa_app_logo.jpg',
       description:'A single-page application is a type of web application that loads a single initial page from the server and then dynamically updates its content, through user interactions, without the need to load additional pages from the server.',
-      href:'http://app.angular.tecappsys.com/spa'
+      href: GLOBALS_PATH.SPA
     }
   ]
 
   public constructor(){}
 
   public navigateToApps(urlPath:string){
-    setTimeout(() => {
+    setTimeout(() => {      
+      //window.open(GLOBALS_PATH.SPA, '_blank')
       window.location.href = urlPath
     }, 100);
   }
