e6291a3d6e1fbb2519f8754f568e1637dd83e9e8
Daniel <danieldiazmc@gmail.com>
2024-01-03 09:12:12 -0300

    add enviroment

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { GLOBALS_PATH } from '@src/app/shared/enums/globals-path.enum';
+import { environment } from '@src/environments/environment.dev';
 
 @Component({
   selector: 'app-home',
@@ -15,7 +14,7 @@ export class HomeComponent {
       imgPath:'../../assets/img/angular_spa_app.jpg',
       imgLogoPath:'../../assets/img/angular_spa_app_logo.jpg',
       description:'A single-page application is a type of web application that loads a single initial page from the server and then dynamically updates its content, through user interactions, without the need to load additional pages from the server.',
-      href: GLOBALS_PATH.SPA
+      href: environment.app_spa
     }
   ]
 
@@ -23,7 +22,6 @@ export class HomeComponent {
 
   public navigateToApps(urlPath:string){
     setTimeout(() => {      
-      //window.open(GLOBALS_PATH.SPA, '_blank')
       window.location.href = urlPath
     }, 100);
   }
