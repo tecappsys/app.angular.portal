aec6db17cd0440bcb621cad388fab603711aac2f
Daniel <danieldiazmc@gmail.com>
2025-03-01 19:57:03 -0400

    ajuste enviroments

--- src/app/core/app.service.ts
+++ src/app/core/app.service.ts
@@ -12,7 +12,7 @@ export class AppService{
           imgPath:'../../assets/img/angular_spa_app.jpg',
           imgLogoPath:'../../assets/img/angular_spa_app_logo.jpg',
           description:'A single-page application is a type of web application that loads a single initial page from the server and then dynamically updates its content, through user interactions, without the need to load additional pages from the server.',
-          href: environment.app_spa
+          href: environment.appSpa
         },
         {
           title:'Spotify',
@@ -20,7 +20,7 @@ export class AppService{
           imgPath:'../../assets/img/angular_spotify_app.jpg',
           imgLogoPath:'../../assets/img/angular_spotify_app_logo.jpg',
           description:'Application with use of spotify public apis, token management, get requests, get music from artists, use spotify widget to play the song, among other things.',
-          href: environment.app_spotify
+          href: environment.appSpotify
         }
       ]
    
